b7255a2e14fcef3e58183381ed3efbb40ba54be8
Jean-Paul NGALULA <jeanpaulngalula07@gmail.com>
2025-05-19 16:45:59 +0200

    feat: player A scores first point and gets 15

--- src/main/java/com/tennis/TennisGame.java
+++ src/main/java/com/tennis/TennisGame.java
@@ -1,7 +1,27 @@
 package com.tennis;
 
 public class TennisGame {
+  private int playerAScore = 0;
+  private int playerBScore = 0;
+
+  private static final int[] SCORE_TABLE = {0, 15, 30, 40};
+
+  /**
+   * Returns the current score of the game.
+   */
   public String getScore() {
-    return "Player A : 0 / Player B : 0";
+    return "Player A : " + SCORE_TABLE[playerAScore] + " / Player B : " + SCORE_TABLE[playerBScore];
+  }
+
+  /**
+   * Called when a player wins a point.
+   * @param player 'A' or 'B'
+   */
+  public void pointWonBy(char player) {
+    if (player == 'A') {
+      playerAScore++;
+    } else if (player == 'B') {
+      playerBScore++;
+    }
   }
 }
--- src/test/java/com/tennis/TennisGameTest.java
+++ src/test/java/com/tennis/TennisGameTest.java
@@ -9,4 +9,11 @@ public class TennisGameTest {
     TennisGame game = new TennisGame();
     assertEquals("Player A : 0 / Player B : 0", game.getScore());
   }
+
+  @Test
+  void playerA_scores() {
+    TennisGame game = new TennisGame();
+    game.pointWonBy('A');
+    assertEquals("Player A : 15 / Player B : 0", game.getScore());
+  }
 }
